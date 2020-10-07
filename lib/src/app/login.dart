90cdbc3b074aab49a933773c5bd4436df23f363a
gilvano <g.piontkoski@gmail.com>
2020-10-07 09:07:42 -0300

    ajuste na tela de login

--- lib/src/app/login.dart
+++ lib/src/app/login.dart
@@ -10,30 +10,62 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  EditWidget('Email'),
-                ],
-              ),
+        backgroundColor: Colors.white,
+        body: Container(
+          child: Padding(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      EditWidget('Email'),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      EditWidget('Senha'),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  margin: EdgeInsets.only(top: 20),
+                  child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(3.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Entrar",
+                        ),
+                      ),
+                      onPressed: () {}),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Icon(Icons.tune,
+                            color: Colors.grey,
+                            size: 24.0,
+                            semanticLabel: 'Configurações do servidor'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Container(
-              child: Icon(
-                Icons.tune,
-                color: Colors.grey,
-                size: 24.0,
-                semanticLabel: 'Configurações do servidor'
-              ),
-            ),
-          ],
-        ), 
-      )
-    );
+          ),
+        ));
   }
 }
